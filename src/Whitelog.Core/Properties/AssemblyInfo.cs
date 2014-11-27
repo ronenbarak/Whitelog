f4375ce3da59e7dc277d8ef8afb458a0b3915e75
ronenbarak <ronen_barak@yahoo.com>
2014-11-27 21:10:48 +0200

    adding nuget into into git

--- src/Whitelog.Core/Properties/AssemblyInfo.cs
+++ src/Whitelog.Core/Properties/AssemblyInfo.cs
@@ -1,12 +1,12 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Whitelog.Core")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription(@"Whitelog is a next generation logging library
+Open source, Fluent, Hierarchical,Binary and String")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Whitelog")]
 [assembly: AssemblyProduct("Whitelog.Core")]
