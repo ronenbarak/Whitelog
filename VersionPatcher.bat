1b5c6943d5036422f6cf210a8f8b5de55587f7f1
ronenbarak <ronen_barak@yahoo.com>
2014-11-27 21:19:53 +0200

    Version patcher auto incrementate

--- VersionPatcher.bat
+++ VersionPatcher.bat
@@ -1,1 +1,1 @@
-src\Utilities\Barak.VersionPatcher.exe /vcPath:master /vcUrl:https://github.com/ronenbarak/Whitelog.git /fsPath:%~dp0 /commit:false /recursive:true
+src\Utilities\Barak.VersionPatcher.exe /vcPath:master /vcUrl:https://github.com/ronenbarak/Whitelog.git /fsPath:%~dp0 /commit:true /recursive:true
